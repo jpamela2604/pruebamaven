e622f3ec2dff035650bf36468991247c5eca9b7e
Pamela Palacios <Pamela Palacios@PALACIOS>
2018-01-28 15:33:05 -0600

    Se agrego prueba Funcional

---
+++ pruebaMaven/src/test/java/PruebasFuncionales/PruebaFuncional.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PruebasFuncionales;
+
+import com.eviware.soapui.tools.SoapUITestCaseRunner;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pamela Palacios
+ */
+public class PruebaFuncional {
+    
+    public PruebaFuncional() {
+    }
+     @Test
+	public void shouldExecuteAllTestCases() throws Exception{
+		// given
+		SoapUITestCaseRunner runner = new SoapUITestCaseRunner();
+                String path="C:\\Users\\Pamela Palacios\\Desktop\\pruebafuncional-soapui-project.xml";
+                runner.setProjectFile(path);
+        	//runner.setProjectProperties(properties);
+        	runner.run();
+	}
+}
